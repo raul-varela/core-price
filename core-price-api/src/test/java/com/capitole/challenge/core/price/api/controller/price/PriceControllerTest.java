d11a474849d6136871c38261cba4fa67b1c1429c
Raúl Varela <68540728+raul-varela@users.noreply.github.com>
2022-12-11 15:43:24 -0300

    production v1.0.1 (#25)
    * added modules (#1)
    
    * CU price (#8)
    
    * jwt feature (#9)
    
    * add flyway config & sqls (#10)
    
    * added testcases to price-api (#11)
    
    * test security controller (#12)
    
    * add swagger support (#16)
    
    * add swagger support
    
    * update swagger
    
    * added Dockerfile (#18)
    
    * update port (#19)
    
    * added documentation (#14)
    
    * added documentation
    
    * update Readme
    
    * added remote url
    
    * add docker info
    
    * fix checkstyle (#20)
    
    * update railway url (#21)
    
    * added new test (#23)
    
    * Feature/add endpoints (#24)
    
    * add documentation & endpoinst
    
    * add documentation

--- core-price-api/src/test/java/com/capitole/challenge/core/price/api/controller/price/PriceControllerTest.java
+++ core-price-api/src/test/java/com/capitole/challenge/core/price/api/controller/price/PriceControllerTest.java
@@ -1,12 +1,14 @@
 package com.capitole.challenge.core.price.api.controller.price;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.anyLong;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,9 +16,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import com.capitole.challenge.core.price.api.controller.price.PriceController;
 import com.capitole.challenge.core.price.domain.entity.Brand;
 import com.capitole.challenge.core.price.domain.entity.ECurrency;
 import com.capitole.challenge.core.price.domain.entity.PriceList;
@@ -32,6 +38,35 @@ class PriceControllerTest {
 
   @InjectMocks
   PriceController priceController;
+
+
+  @Test
+  @DisplayName("Should return a list of prices when there are prices in the database")
+  void getAllWhenThereArePricesInTheDatabaseThenReturnAListOfPrices() {
+    PriceList priceList =
+            PriceList.builder()
+                    .id(1L)
+                    .brand(Brand.builder().id(1L).name("Brand 1").build())
+                    .product(Product.builder().id(1L).name("Product 1").build())
+                    .startDate(LocalDateTime.now())
+                    .endDate(LocalDateTime.now())
+                    .priority(1)
+                    .price(10.0)
+                    .currency(ECurrency.EUR)
+                    .build();
+
+    Pageable pageable = PageRequest.of(0, 50);
+    List<PriceList> listPrice = Arrays.asList(priceList);
+    Page<PriceList> priceListPage = new PageImpl<>(Collections.singletonList(priceList));
+
+    when(priceRepository.findAll(pageable)).thenReturn(priceListPage);
+
+
+    ResponseEntity<?> rta = priceController.getAll();
+
+    assertEquals(HttpStatus.OK, rta.getStatusCode());
+    assertEquals(priceListPage, rta.getBody());
+  }
 
   @Test
   @DisplayName("Should return no price when the product and date are invalid")
@@ -79,11 +114,11 @@ class PriceControllerTest {
 
     when(priceRepository.getPriceListByBrandAndProductAndBetweenDates(
             anyLong(), anyLong(), any(), any()))
-            .thenReturn(Collections.singletonList(priceList));
+            .thenReturn(Arrays.asList(priceList));
 
     var response = priceController.getPriceByProductAndDate(requestPriceList);
 
     assertEquals(HttpStatus.OK, response.getStatusCode());
-    assertEquals(Collections.singletonList(priceList), response.getBody());
+    assertEquals(Arrays.asList(priceList), response.getBody());
   }
 }
