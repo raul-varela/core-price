a8e562b7d5b639d22b072a161772700b5636e46a
Raúl Varela <68540728+raul-varela@users.noreply.github.com>
2022-12-11 15:23:12 -0300

    Feature/add endpoints (#24)
    * add documentation & endpoinst
    
    * add documentation

--- core-price-api/src/main/java/com/capitole/challenge/core/price/api/controller/price/PriceController.java
+++ core-price-api/src/main/java/com/capitole/challenge/core/price/api/controller/price/PriceController.java
@@ -3,9 +3,11 @@ package com.capitole.challenge.core.price.api.controller.price;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,4 +36,16 @@ public class PriceController {
       return ResponseEntity.ok("No Price Available");
     }
   }
+
+  @GetMapping("/")
+  @PreAuthorize("hasAuthority('USER') or hasAuthority('MODERATOR') or hasAuthority('ADMIN')")
+  public ResponseEntity<?> getAll() {
+    Page<PriceList> priceList = priceRepository.findAll(PageRequest.of(0, 50));
+    if (!priceList.isEmpty()) {
+      return ResponseEntity.ok(priceList);
+    } else {
+      return ResponseEntity.ok("No Price Available");
+    }
+  }
+
 }
